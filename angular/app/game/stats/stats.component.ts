6878f677890890b8a263a6c75e359666ddfe68c4
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2016-08-28 22:42:17 +0200

    some style updates

--- angular/app/game/stats/stats.component.ts
+++ angular/app/game/stats/stats.component.ts
@@ -5,6 +5,7 @@ import {StatsService} from './stats.service'
 @Component({
   selector: "stats",
   templateUrl: "app/game/stats/stats.component.html",
+  styleUrls: ["app/game/stats/stats.component.css"],
   providers: [
     StatsService
   ]
