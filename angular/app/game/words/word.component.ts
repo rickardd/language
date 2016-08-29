7382b43124a74b664bf48dd5d71ce43ffef25b16
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2016-08-29 17:17:21 +0200

    style updates

--- angular/app/game/words/word.component.ts
+++ angular/app/game/words/word.component.ts
@@ -9,7 +9,7 @@ import {GameService} from '../game.service'
 @Component({
   selector: "words",
   templateUrl: "app/game/words/word.component.html",
-  styleUrls: ["app/game/words/style.css"],
+  styleUrls: ["app/game/words/word.component.css"],
   providers: [
     GameService
   ]
