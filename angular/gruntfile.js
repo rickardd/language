dc26fbb36af93c4be238360e2798af5b4d589af3
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2017-01-31 13:28:53 +1300

    start: Basic design - profile - and scss settup.

--- angular/gruntfile.js
+++ angular/gruntfile.js
@@ -69,7 +69,7 @@ module.exports = function (grunt) {
       //   }
       // },
       css: {
-        files: ['app/**/*.scss'],
+        files: ['**/*.scss'],
         //tasks: ['sass', 'kss'],
         tasks: ['sass'],
         options: {
